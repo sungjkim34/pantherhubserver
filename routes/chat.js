be92e05c33a19cba66ccd853cb7d8957c6d26d09
sungjkim34 <sungjkim34@gmail.com>
2018-02-05 22:28:48 -0500

    add delete chat

--- routes/chat.js
+++ routes/chat.js
@@ -7,6 +7,16 @@ module.exports = function(app, con, io, moment){
         });
     });
 
+    //TODO
+    app.get('/deleteMessage/:messageId', function(req, res) {
+        var messageId = req.params.messageId;
+        var sql = 'DELETE FROM chats WHERE id = ' + messageId;
+        con.query(sql, (err, result, fields) => {
+            if(err) res.send(result);
+            res.send(result);
+        });
+    });
+
     io.on('connection', socket => {
         socket.on('sendMessage', message => {
             const { messageText, authorId, authorType, authorFirstName, authorLastName } = message;
@@ -14,7 +24,8 @@ module.exports = function(app, con, io, moment){
             var sql = 'INSERT INTO chats (messageText, messageDate, authorId, authorType, authorFirstName, authorLastName) VALUES (\'' + messageText + '\', \'' + messageDate + '\', \'' + authorId + '\', \'' + authorType + '\', \'' + authorFirstName + '\', \'' + authorLastName + '\')';
             con.query(sql, (err, result) => {
                 if(err) console.log(err);
-                console.log(result);
+                message.messageDate = messageDate;
+                message.id = result.insertId;
                 const sendMessage = Object.assign({}, {socketId: socket.id, message});
                 io.sockets.emit('sendMessage', sendMessage);
             });
