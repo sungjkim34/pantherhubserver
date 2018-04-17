7417cd4a80b96f9616d90545e576e40db48f9130
sungjkim34 <sungjkim34@gmail.com>
2018-04-17 10:48:29 -0400

    changes to students

--- routes/students.js
+++ routes/students.js
@@ -57,4 +57,39 @@ module.exports = function(app, con, logger){
             // res.send(result);
         });
     });
+
+    // Address
+    app.get('/getStudentContactInfo/:studentId', function(req, res) {
+        var studentId = req.params.studentId;
+        var sql = 'SELECT * FROM contactInfos WHERE studentId = ' + studentId;
+        con.query(sql, (err, result, fields) => {
+            if (err) {
+                logger.debug(err);
+                res.send(err);
+            }
+            res.send(result && result.length ? result[0] : result);
+        });
+    });
+    
+    app.post('/updateContactInfo', function(req, res) {
+        var contactInfo = {
+            studentId: req.body.studentId,
+            street: req.body.street,
+            street2: req.body.street2,
+            city: req.body.city,
+            state: req.body.state,
+            postalCode: req.body.postalCode,
+            phoneNumber: req.body.phoneNumber,
+            email: req.body.email
+        };
+        var sql = 'INSERT INTO contactInfos (studentId, street, street2, city, state, postalCode, phoneNumber, email) VALUES(' + contactInfo.studentId + ', \'' + contactInfo.street + '\', \'' + contactInfo.street2 + '\', \'' + contactInfo.city + '\', \'' + contactInfo.state + '\', ' + contactInfo.postalCode + ', \'' + contactInfo.phoneNumber + '\', \'' + contactInfo.email + '\') ON DUPLICATE KEY UPDATE  studentId=VALUES(studentId), street=VALUES(street), street2=VALUES(street2), city=VALUES(city), state=VALUES(state), postalCode=VALUES(postalCode), phoneNumber=VALUES(phoneNumber), email=VALUES(email)';
+        // res.send(sql);
+        con.query(sql, (err, result) => {
+            if (err) {
+                logger.debug(err);
+                res.send(err);
+            }
+            res.send(result);
+        });
+    });
 }
